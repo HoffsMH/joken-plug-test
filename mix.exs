9a3490856c01fd640c22cf4c175b649192b431e8
HoffsMH <hecker.matthew@gmail.com>
2018-07-17 08:25:31 -0400

    just use local joken plug for now for faster dev

--- mix.exs
+++ mix.exs
@@ -32,6 +32,7 @@ defmodule JokenPlugTest.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:joken_plug, path: "../joken-plug"},
       {:phoenix, "~> 1.3.3"},
       {:phoenix_pubsub, "~> 1.0"},
       {:gettext, "~> 0.11"},
